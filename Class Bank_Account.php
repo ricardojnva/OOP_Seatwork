34d0528095cd7dbca1c6a309fd765a4e75abb45d
ricardojnva <143089452+ricardojnva@users.noreply.github.com>
2023-10-17 19:33:26 -0700

    Add files via upload
    SI DENVER SOMERA PO KASAMA KO SIR

---
+++ Class Bank_Account.php
@@ -0,0 +1,35 @@
+<?php
+require 'Person.php';
+$person = new Person($conn);
+$persons = $person->readPersons();
+ // DENVER SOMERA //
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bank Display</title>
+</head>
+<body>
+    <h1>Bank Account</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Balance</th>
+            <th>Account_Type</th>
+        </tr>
+        
+        <?php foreach ($persons as $person) { ?>
+
+        <tr>
+            <td><?php echo $person['account_ID']; ?></td>
+            <td><?php echo $person['account_name']; ?></td>
+            <td><?php echo $person['balance']; ?></td>
+            <td><?php echo $person['account_type']; ?></td>
+            
+        </tr>
+        <?php } ?>
+    </table>
+    </body>
+</html>
---
+++ Person.php
@@ -0,0 +1,102 @@
+<?php
+require 'DB_config.php';
+
+class Person {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+ // JANEVA RICARDO //
+    public function createPerson($Account_Name , $Balance , $Account_Type) {
+        $Account_Name  = mysqli_real_escape_string($this->conn, $Account_Name);
+        $Balance = (int)$Balance;
+        $Account_Type = mysqli_real_escape_string($this->conn, $Account_Type);
+        
+        $query = "INSERT INTO bank_account (account_name, balance, account_type) VALUES ('$Account_Name', '$Balance', '$Account_Type')";
+        return mysqli_query($this->conn, $query);
+    }
+    public function readPersons() {
+        $query = "SELECT * FROM bank_account";
+        $result = mysqli_query($this->conn, $query);
+
+        $persons = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $persons[] = $row;
+        }
+
+        return $persons;
+    }
+    public function withdraw($Account_Name, $Balance) {
+        $Account_Name = mysqli_real_escape_string($this->conn, $Account_Name);
+        $Balance = (int)$Balance;
+    
+        $query = "SELECT * FROM bank_account WHERE account_name = '$Account_Name'";
+        $result = mysqli_query($this->conn, $query);
+    
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $current_balance = $row['balance'];
+    
+            if ($current_balance >= $Balance) {
+                $new_balance = $current_balance - $Balance;
+    
+                $update_query = "UPDATE bank_account SET balance = $new_balance WHERE account_name = '$Account_Name'";
+                $update_result = mysqli_query($this->conn, $update_query);
+    
+                if ($update_result) {
+                    return "$Account_Name. New balance is $new_balance";
+                } else {
+                    return "Error updating balance";
+                }
+            } else {
+                return "Insufficient funds";
+            }
+        } else {
+            return "Account not found";
+        }
+    }
+    public function deposite($Account_Name, $Balance) {
+        $Account_Name = mysqli_real_escape_string($this->conn, $Account_Name);
+        $Balance = (int)$Balance;
+    
+        $query = "SELECT * FROM bank_account WHERE account_name = '$Account_Name'";
+        $result = mysqli_query($this->conn, $query);
+    
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $current_balance = $row['balance'];
+    
+            if ($current_balance >= $Balance) {
+                $new_balance = $current_balance + $Balance;
+    
+                $update_query = "UPDATE bank_account SET balance = $new_balance WHERE account_name = '$Account_Name'";
+                $update_result = mysqli_query($this->conn, $update_query);
+    
+                if ($update_result) {
+                    return "$Account_Name . New balance is $new_balance";
+                } else {
+                    return "Error updating balance";
+                }
+            } else {
+                return "Insufficient funds";
+            }
+        } else {
+            return "Account not found";
+        }
+    }
+    public function getUserDetails($Account_Name) {
+        $Account_Name = mysqli_real_escape_string($this->conn, $Account_Name);
+        $query = "SELECT * FROM bank_account WHERE account_name = '$Account_Name'";
+        $result = mysqli_query($this->conn, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            return mysqli_fetch_assoc($result);
+        } else {
+            return null;
+        }
+    }
+    
+
+}
+?>
